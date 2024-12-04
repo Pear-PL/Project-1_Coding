0fe1d9af0ee1fa674859d71c75dd666b4fc4e7b6
Pear-PL <lalaloveshu@gmail.com>
2024-12-04 16:07:08 +0700

    Add files via upload

---
+++ 1form_31pear.py
@@ -0,0 +1,10 @@
+print ("Welcome to Homework Checking")
+
+print (" กรุณากรอกข้อมูล ")
+nickname = input (" ชื่อเล่น ")
+number = input (" เลขที่ ")
+subject = input (" การบ้านของวิชา ")
+
+print ("\n\nยินดีต้อนรับ ", nickname)
+print ("คุณกำลังเช็คการบ้านรายวิชา ", subject)
+print ("\n\nอย่าลืมทำการบ้านส่งให้ตรงเวลา ")
